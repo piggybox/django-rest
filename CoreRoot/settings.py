e6183395e1a82c28e02dfe97fca58a2ca42f3a5e
Sky Yin <sky.yin@gmail.com>
2024-12-22 18:14:23 -0800

    Add user and superuser

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'core'
+    'core',
+    'core.user'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core_user.User'
